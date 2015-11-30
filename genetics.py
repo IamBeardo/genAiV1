59346d4986b09e7af97b10f86140e726d9f16ec1
Adam <adam@rajraj.sae>
2015-11-30 23:20:43 +0100

    ,,

--- genetics.py
+++ genetics.py
@@ -8,6 +8,7 @@ class Gen:
     mycount=0
 
     def __init__(self,genformat=string.digits,genlen=10):
+        self.fittnes=0
         self.genformat=genformat
         self.genlen=genlen
         self.body = ''.join(random.choice(self.genformat) for i in xrange(genlen))
@@ -19,6 +20,19 @@ class Gen:
     def __del__(self):
         Gen.mycount-=1
         #print Gen.mycount
+    def calcFittnes(self,target="012345"):
+        tmp=0
+        for i  in range(len(self.body)):
+
+            tmp2= ord(target[i])-ord(self.body[i])
+            tmp2=tmp2*tmp2
+            tmp = tmp2+tmp
+            print tmp,tmp2
+        #tFittnes+= ord(x)-ord(y)
+        print(target,self.body)
+
+        self.fittnes=tmp
+
 
 
 
@@ -34,9 +48,12 @@ class Individual:
 
 
 class Population:
-    def __init__(self):
-        pass
 
+    def __init__(self,nrofindividuals):
+        self.individuals = [Gen() for i in xrange(nrofindividuals)]
+        pass
+    def __repr__(self):
+        return "".join("["+str(i)+"] " for i in self.individuals)
 
 
 
--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import string
 
 
 
-random.seed("YouAreNotAlone")
+random.seed("YouAreNotAlone...")
 
 a=genetics.Gen(string.digits,6)
 b=genetics.Gen(string.digits,6)
 c=genetics.Gen(string.digits,6)
 
-print a.body
-print b.mycount
 
 
 
 
+
+world=genetics.Population(10)
+
+
+
+a.calcFittnes()
+print a.fittnes
