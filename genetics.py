6fa471740f0993b178d654507ff25550c5d6fba1
IamBeardo <adam@rajraj.se>
2015-12-01 17:00:52 +0100

    test update from Work

--- genetics.py
+++ genetics.py
@@ -3,14 +3,31 @@ import random
 
 
 
-
 class Gen:
-    mycount=0
+    mycount = 0
 
-    def __init__(self,genformat=string.digits,genlen=10):
-        self.fittnes=0
-        self.genformat=genformat
-        self.genlen=genlen
+    def calcFitness(self, target=""):
+        cmpString=self.body
+        if target == "": target="012345"
+
+        #print "target {} {}".format(target,len(target))
+        #print "self.body {} {}".format(cmpString,len(cmpString))
+
+        tmp = 0
+        for i in range(len(cmpString)):
+            tmp2 = ord(target[i]) - ord(cmpString[i])
+            tmp2 *= tmp2  # ^2 to increase fitness in bigger diff and convert to unsigned
+            tmp += tmp2
+            print tmp, tmp2
+            #tFittnes+= ord(x)-ord(y)
+        self.fitness=tmp
+        print(target, cmpString,self.fitness)
+        return cmpString
+
+    def __init__(self, genformat=string.digits, genlen=10):
+        self.fitness = 0
+        self.genformat = genformat
+        self.genlen = genlen
         self.body = ''.join(random.choice(self.genformat) for i in xrange(genlen))
         Gen.mycount += 1
 
@@ -18,43 +35,37 @@ class Gen:
         return self.body
 
     def __del__(self):
-        Gen.mycount-=1
+        Gen.mycount -= 1
         #print Gen.mycount
-    def calcFittnes(self,target="012345"):
-        tmp=0
-        for i  in range(len(self.body)):
 
-            tmp2= ord(target[i])-ord(self.body[i])
-            tmp2=tmp2*tmp2
-            tmp = tmp2+tmp
-            print tmp,tmp2
-        #tFittnes+= ord(x)-ord(y)
-        print(target,self.body)
 
-        self.fittnes=tmp
 
 
 
 
 class Individual:
-    mycount=0
+    mycount = 0
 
-    def __init__():
-        Individual.mycount +=1
+    def __init__(self):
+        Individual.mycount += 1
 
     def __del__(self):
-        Individual.mycount -=1
-
+        Individual.mycount -= 1
 
 
 class Population:
+    def __init__(self, nrofindividuals):
+        self.individuals = [Gen(genlen=6) for i in xrange(nrofindividuals)]
+        print "setting up population..."
 
-    def __init__(self,nrofindividuals):
-        self.individuals = [Gen() for i in xrange(nrofindividuals)]
-        pass
+
     def __repr__(self):
-        return "".join("["+str(i)+"] " for i in self.individuals)
+        return "".join("[" + str(i) + "] " for i in self.individuals)
 
+    def calcFitness(self,target=""):
+        for i in self.individuals.__getslice__(2,3):
+            i.calcFitness(target)
+            print i.fitness
 
 
 
--- main.py
+++ main.py
@@ -3,21 +3,9 @@ import random
 import string
 
 
-
 random.seed("YouAreNotAloneWhenAtWork...")
 
-a=genetics.Gen(string.digits,6)
-b=genetics.Gen(string.digits,6)
-c=genetics.Gen(string.digits,6)
 
 
-
-
-
-
-world=genetics.Population(10)
-
-
-
-a.calcFittnes()
-print a.fittnes
+world = genetics.Population(10)
+world.calcFitness("012345")
